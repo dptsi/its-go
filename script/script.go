b74a5208357974d9b28a83622ec276118e3b6c34
Zydhan Linnar Putra <zydhanlinnar11@gmail.com>
2024-01-03 11:59:14 +0700

    feat(script): base service

---
+++ script/script.go
@@ -0,0 +1,55 @@
+package script
+
+import (
+	"fmt"
+	"os"
+
+	"bitbucket.org/dptsi/its-go/contracts"
+)
+
+type ScriptService struct {
+	commands map[string]contracts.ScriptCommand
+}
+
+func NewScriptService() *ScriptService {
+	return &ScriptService{
+		commands: make(map[string]contracts.ScriptCommand),
+	}
+}
+
+func (s *ScriptService) AddCommand(c contracts.ScriptCommand) error {
+	if _, isExist := s.commands[c.Key()]; isExist {
+		return fmt.Errorf("command key %s for %s already exist", c.Key(), c.Name())
+	}
+	s.commands[c.Key()] = c
+
+	return nil
+}
+
+func (s *ScriptService) Run() error {
+	args := os.Args[1:]
+
+	if len(args) == 0 || args[0] == "help" {
+		fmt.Println("Available commands:")
+		fmt.Println()
+
+		for _, cmd := range s.commands {
+			fmt.Printf("======== %s ========\n", cmd.Name())
+			fmt.Printf("Description\t: %s\n", cmd.Name())
+			fmt.Printf("Usage\t\t: %s\n", cmd.Usage())
+			fmt.Println()
+		}
+
+		if len(s.commands) == 0 {
+			fmt.Println("- No command available yet.")
+		}
+
+		return nil
+	}
+
+	command, exist := s.commands[args[0]]
+	if !exist {
+		return fmt.Errorf("command %s not found\nrun this script without arguments or with argument \"help\" to show help", args[0])
+	}
+	return command.Handler()(args[1:])
+}
